5b80d4f526efc72e9231859e4efbe3365b119100
StojcevskiD <dimitar.stojcevski@hotmail.com>
2022-02-02 17:30:25 +0100

    subject file upload

--- src/components/Subject/Subject.js
+++ src/components/Subject/Subject.js
@@ -2,37 +2,158 @@ import React, {useEffect} from "react";
 import {Link, useParams} from "react-router-dom";
 import './Subject.css'
 import SubjectService from "../../repository/SubjectRepository";
+import Form from 'react-bootstrap/Form'
+import {Button} from "react-bootstrap";
 
 
 const Subject = () => {
+
     const id = parseInt(useParams().id)
 
-    const [subject, setSubject] = React.useState();
+    const [subject, setSubject] = React.useState()
+    const [files1, setFiles1] = React.useState([])
+    const [files2, setFiles2] = React.useState([])
+    const [files3, setFiles3] = React.useState([])
+    const [inputButtonDisable1, setInputButtonDisable1] = React.useState(true)
+    const [inputButtonDisable2, setInputButtonDisable2] = React.useState(true)
+    const [inputButtonDisable3, setInputButtonDisable3] = React.useState(true)
+
+
     useEffect(() => {
         SubjectService.getSubjectById(id).then((s) => {
             setSubject(s.data)
-            console.log("sub", s.data)
+            // console.log("sub", s.data)
         })
 
     }, [id])
+
+    const addMaterials = () => {
+
+    }
+
+    const fileChange = (e) => {
+        const id = e.target.id
+        const fil = e.target.files
+
+        if (id === "first_input") {
+            setFiles1(fil)
+            if (fil.length === 0) {
+                setInputButtonDisable1(true)
+            } else {
+                setInputButtonDisable1(false)
+            }
+        } else if (id === "second_input") {
+            setFiles2(fil)
+            if (fil.length === 0) {
+                setInputButtonDisable2(true)
+            } else {
+                setInputButtonDisable2(false)
+            }
+        } else if (id === "third_input") {
+            setFiles3(fil)
+            if (fil.length === 0) {
+                setInputButtonDisable3(true)
+            } else {
+                setInputButtonDisable3(false)
+            }
+        }
+    }
+
+    const showForm = (e) => {
+        if (e.target.id === "first_button") {
+            if (document.getElementById("first_form_div").style.display === "none") {
+                document.getElementById("first_form_div").style.display = "block"
+            } else {
+                document.getElementById("first_form_div").style.display = "none"
+            }
+        } else if (e.target.id === "second_button") {
+            if (document.getElementById("second_form_div").style.display === "none") {
+                document.getElementById("second_form_div").style.display = "block"
+            } else {
+                document.getElementById("second_form_div").style.display = "none"
+            }
+        } else {
+            if (document.getElementById("third_form_div").style.display === "none") {
+                document.getElementById("third_form_div").style.display = "block"
+            } else {
+                document.getElementById("third_form_div").style.display = "none"
+            }
+        }
+    }
 
     return (
         <div className="container">
             {subject !== undefined ?
                 <div className="row">
                     <h1 id="subject_title">{subject.name}</h1>
-                    <div className="col-12 col-lg-4">
+                    <div className="col-12 col-md-4 subject_sub_title_border_right">
                         <h3 className="subject_sub_title">Прв колоквиум</h3>
+                        {/*<div>*/}
+                        {/*    <button className="btn btn-outline-danger">Избриши материјали</button>*/}
+                        {/*</div>*/}
+                        <div>
+                            <button className="btn btn-outline-primary subject_add_button" id="first_button"
+                                    onClick={showForm}>Додади материјали
+                            </button>
+                        </div>
+                        <div id="first_form_div" style={{display: "none"}}>
+                            <Form.Group className="mb-3 subject_form_group">
+                                <Form.Label><h5>-Додади материјали</h5></Form.Label>
+                                <Form.Control type="file" multiple onChange={fileChange} id="first_input"/>
+                            </Form.Group>
+                            <button className="btn btn-outline-primary" onClick={addMaterials}
+                                    disabled={inputButtonDisable1}>Додади
+                            </button>
+                        </div>
 
+                        <h5 className="subject_empty_text">Моментално нема материјали за овој дел</h5>
                     </div>
 
-                    <div className="col-12 col-lg-4 subject_sub_title_border">
+                    <div className="col-12 col-md-4 subject_sub_title_border_left subject_sub_title_border_right">
                         <h3 className="subject_sub_title">Втор колоквиум</h3>
+                        {/*<div>*/}
+                        {/*    <button className="btn btn-outline-danger">Избриши материјали</button>*/}
+                        {/*</div>*/}
+                        <div>
+                            <button className="btn btn-outline-primary subject_add_button" id="second_button"
+                                    onClick={showForm}>Додади материјали
+                            </button>
+                        </div>
+                        <div id="second_form_div" style={{display: "none"}}>
+                            <Form.Group className="mb-3 subject_form_group">
+                                <Form.Label><h5>-Додади материјали</h5></Form.Label>
+                                <Form.Control type="file" multiple onChange={fileChange} id="second_input"/>
+                            </Form.Group>
+                            <button className="btn btn-outline-primary" onClick={addMaterials}
+                                    disabled={inputButtonDisable2}>Додади
+                            </button>
+                        </div>
+
+                        <h5 className="subject_empty_text">Моментално нема материјали за овој дел</h5>
 
                     </div>
 
-                    <div className="col-12 col-lg-4 subject_sub_title_border">
+                    <div className="col-12 col-md-4 subject_sub_title_border_left">
                         <h3 className="subject_sub_title">Испит</h3>
+                        {/*<div>*/}
+                        {/*    <button className="btn btn-outline-danger">Избриши материјали</button>*/}
+                        {/*</div>*/}
+                        <div>
+                            <button className="btn btn-outline-primary subject_add_button" id="third_button"
+                                    onClick={showForm}>Додади материјали
+                            </button>
+                        </div>
+                        <div id="third_form_div" style={{display: "none"}}>
+                            <Form.Group className="mb-3 subject_form_group">
+                                <Form.Label><h5>-Додади материјали</h5></Form.Label>
+                                <Form.Control type="file" multiple onChange={fileChange} id="third_input"/>
+                            </Form.Group>
+                            <button className="btn btn-outline-primary" onClick={addMaterials}
+                                    disabled={inputButtonDisable3}>Додади
+                            </button>
+                        </div>
+
+                        <h5 className="subject_empty_text">Моментално нема материјали за овој дел</h5>
 
                     </div>
                 </div>

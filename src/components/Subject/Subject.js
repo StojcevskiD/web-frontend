33034e803d4086cd5b39edcbc2daa09cefe2652f
StojcevskiD <dimitar.stojcevski@hotmail.com>
2022-02-06 16:13:57 +0100

    file upload done

--- src/components/Subject/Subject.js
+++ src/components/Subject/Subject.js
@@ -13,7 +13,7 @@ const Subject = () => {
     const id = parseInt(useParams().id)
 
     const [subject, setSubject] = React.useState()
-    const [files1, setFiles1] = React.useState()
+    const [files1, setFiles1] = React.useState([])
     const [files2, setFiles2] = React.useState([])
     const [files3, setFiles3] = React.useState([])
     const [inputButtonDisable1, setInputButtonDisable1] = React.useState(true)
@@ -29,10 +29,11 @@ const Subject = () => {
 
     }, [id])
 
-    const addMaterials = () => {
-        const formData = new FormData()
-        formData.append('files', files1)
-        // const files = files1
+    const toFormData = (f) => {
+        let formData = new FormData()
+        for (let i = 0; i < f.length; i++) {
+            formData.append("files", f[i])
+        }
 
         Swal.fire({
             title: 'Дали сте сигурни?',
@@ -46,22 +47,31 @@ const Subject = () => {
         }).then((result) => {
             if (result.isConfirmed) {
                 FileService.uploadFile(id, formData).then((result) => {
-                    alert("file uploaded")
+                    Swal.fire(
+                        'Успешно!',
+                        'Фајловите беа успешно прикачени.',
+                        'success'
+                    )
+                }).catch(() => {
+                    Swal.fire(
+                        'Грешка!',
+                        'Големината на фајлот е преголема! Обидете се повторно.',
+                        'error'
+                    )
                 })
-
-                // Swal.fire(
-                //     'Успешно!',
-                //     'Фајловите беа успешно прикачени.',
-                //     'success'
-                // )
             }
-        }).catch(() => {
-            Swal.fire(
-                'Грешка!',
-                'Обидете се повторно.',
-                'error'
-            )
         })
+    }
+
+    const addMaterials = (e) => {
+        const buttonId = e.target.id
+        if (buttonId === "subject_button1") {
+            toFormData(files1)
+        } else if (buttonId === "subject_button2") {
+            toFormData(files2)
+        } else {
+            toFormData(files3)
+        }
     }
 
     const deleteMaterials = (e) => {
@@ -71,29 +81,36 @@ const Subject = () => {
     const fileChange = (e) => {
         const id = e.target.id
         const fil = e.target.files
-
+        let arr = []
+        for (let i = 0; i < fil.length; i++) {
+            arr.push(fil[i])
+        }
         if (id === "first_input") {
-            setFiles1(fil)
             if (fil.length === 0) {
+                setFiles1(fil)
                 setInputButtonDisable1(true)
             } else {
+                setFiles1(arr)
                 setInputButtonDisable1(false)
             }
         } else if (id === "second_input") {
-            setFiles2(fil)
             if (fil.length === 0) {
+                setFiles2(fil)
                 setInputButtonDisable2(true)
             } else {
+                setFiles2(arr)
                 setInputButtonDisable2(false)
             }
         } else if (id === "third_input") {
-            setFiles3(fil)
             if (fil.length === 0) {
+                setFiles3(fil)
                 setInputButtonDisable3(true)
             } else {
+                setFiles3(arr)
                 setInputButtonDisable3(false)
             }
         }
+        console.log("aar", arr)
     }
 
     const showForm = (e) => {
@@ -133,10 +150,10 @@ const Subject = () => {
                         <div id="first_form_div" style={{display: "none"}}>
                             <Form.Group className="mb-3 subject_form_group">
                                 <Form.Label><h5>-Додади материјали</h5></Form.Label>
-                                <Form.Control type="file" onChange={fileChange} id="first_input"/>
+                                <Form.Control type="file" multiple onChange={fileChange} id="first_input"/>
                             </Form.Group>
                             <button className="btn btn-outline-primary" onClick={addMaterials}
-                                    disabled={inputButtonDisable1}>Додади
+                                    disabled={inputButtonDisable1} id="subject_button1">Додади
                             </button>
                         </div>
 
@@ -157,7 +174,7 @@ const Subject = () => {
                                 <Form.Control type="file" multiple onChange={fileChange} id="second_input"/>
                             </Form.Group>
                             <button className="btn btn-outline-primary" onClick={addMaterials}
-                                    disabled={inputButtonDisable2}>Додади
+                                    disabled={inputButtonDisable2} id="subject_button2">Додади
                             </button>
                         </div>
 
@@ -178,7 +195,7 @@ const Subject = () => {
                                 <Form.Control type="file" multiple onChange={fileChange} id="third_input"/>
                             </Form.Group>
                             <button className="btn btn-outline-primary" onClick={addMaterials}
-                                    disabled={inputButtonDisable3}>Додади
+                                    disabled={inputButtonDisable3} id="subject_button3">Додади
                             </button>
                         </div>
 
--- src/repository/FileRepository.js
+++ src/repository/FileRepository.js
@@ -4,16 +4,7 @@ import axios from "../custom-axios/axios";
 const FileService = {
 
     uploadFile: (id, files) => {
-        console.log("id", id)
-        console.log("files", files)
-        return axios.post(`/file/${id}`, {files},
-            {
-                headers:
-                    {
-                        'content-type': 'multipart/form-data',
-                    }
-            }
-        )
+        return axios.post(`/file/${id}`, files)
     }
 
 }

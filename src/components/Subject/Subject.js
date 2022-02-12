edfa65ca53580a65d62a418d106bb45b7e49d1c5
StojcevskiD <dimitar.stojcevski@hotmail.com>
2022-02-12 18:47:51 +0100

    style changes

--- src/components/Subject/Subject.js
+++ src/components/Subject/Subject.js
@@ -59,7 +59,7 @@ const Subject = () => {
             setFilesSecond(arr2)
             setFilesExam(arr3)
         }).then(() => {
-            // setLoading(false)
+            setLoading(false)
         })
     }
 

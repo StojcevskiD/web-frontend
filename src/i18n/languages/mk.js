c318c954b2506db690b0491a7bb31486750ab2b8
Dimitar Stojcevski <dimitar.stojcewski@hotmail.com>
2022-04-07 14:33:49 +0200

    changing language added

---
+++ src/i18n/languages/mk.js
@@ -0,0 +1,12 @@
+const locale = {
+    TITLE: 'ПРЕДМЕТНИК',
+    ALL_SUBJECTS: 'Сите предмети',
+    MY_SUBJECTS: 'Мои предмети',
+    LOG_IN: 'Најави се',
+    LOGOUT: 'Одјави се',
+    REGISTER: 'Регистрирај се',
+    SUBJECTS: 'Предмети',
+
+}
+
+export default locale
